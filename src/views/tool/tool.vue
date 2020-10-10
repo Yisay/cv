da9ff80ec6dca78422ba1e09b25fcebdf3eedfff
office <497615406@qq.com>
2020-10-10 16:58:42 +0800

    2020/10/10 16:58 office

--- src/views/tool/tool.vue
+++ src/views/tool/tool.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>
-
-  </div>
+  <el-container>
+    <el-header></el-header>
+    <el-container>
+      <el-aside width="40%">
+        <el-row :gutter="20" style="height:100%">
+          <el-col :span="8">
+            <el-card></el-card>
+          </el-col>
+          <el-col :span="8">
+            <el-card></el-card>
+          </el-col>
+          <el-col :span="8">
+            <el-card></el-card>
+          </el-col>
+        </el-row>
+      </el-aside>
+      <el-main> </el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
   data () {
-    return {
-
-    }
+    return {}
   }
 }
 </script>
